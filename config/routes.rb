ec5368a314285bdf0f4b5ab542d29681220b02d6
Clement Simons <clementsimons@gmail.com>
2018-02-15 13:02:39 +0100

    defining routes for doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :create, :new]
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    member do
+      resources :doses, only: [:index, :new, :create, :destroy]
+    end
+  end
 end
